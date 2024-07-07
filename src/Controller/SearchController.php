090332cd08ea4bdc4ae00d889ed796abddab7471
Marat Akhmedov <igugle4@gmail.com>
2024-07-07 20:41:53 +0200

    fix error

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -9,7 +9,6 @@ use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bridge\Twig\Attribute\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/search')]
